33750018645cb85a26058be0414cbed142700b56
tomoya baba <tomoya.bb17931837@gmail.com>
2025-01-24 17:42:27 +0900

    start

---
+++ src/test_bb/bb_CF_test.c
@@ -0,0 +1,82 @@
+#include "cub3d.h"
+
+void	free_box (char **box)
+{
+	int	i;
+
+	i = 0;
+	while (box[i])
+	{
+		free(box[i]);
+		i++;
+	}
+	free(box);
+}
+
+bool	is_num(char **list)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (list[i])
+	{
+		j = 0;
+		while (list[i][j])
+		{
+			if (list[i][j] != '\n' && (!ft_isdigit(list[i][j]) || j > 3))
+				return (false);
+			j++;
+		}
+		i++;
+	}
+	printf("hello\n");
+	return (true);
+}
+
+bool	is_wrong_val(char **list)
+{
+	if (!(list[2] && !list[3]) || !is_num(list))
+	{
+		printf("Error\nColor value wrong.\n");
+		free_box(list);
+		return (true);
+	}
+	if ((0 <= ft_atoi(list[0])) && (ft_atoi(list[0]) <= 255) && (0 <= ft_atoi(list[1])) && (ft_atoi(list[1]) <= 255) && (0 <= ft_atoi(list[2])) && (ft_atoi(list[2]) <= 255))
+	{
+		free_box(list);
+		return (false);
+	}
+	printf("Error\nColor value over.\n");
+	free_box(list);
+	return (true);
+}
+
+bool	is_wrong_color(char *line)
+{
+	char	**box;
+	char	**box2;
+
+	if ((line[0] == 'F' || line[0] == 'C') && line[1] == ' ')
+	{
+		box = ft_split(line, ' ');
+		if (!(box[1] && !box[2]))
+		{
+			free_box (box);
+			return (true);
+		}
+		free (box[0]);
+		box2 = ft_split(box[1], ',');
+		free (box[1]);
+		free (box);
+		int i = 0;
+		while (box2[i])
+		{
+			printf("%s\n", box2[i]);
+			i++;
+		}
+		if (is_wrong_val(box2))
+			return (true);
+	}
+	return (false);
+}
---
+++ src/test_bb/bb_name_test.c
@@ -0,0 +1,28 @@
+#include "cub3d.h"
+
+bool	name_check(char *name)
+{
+	int	len;
+
+	len = ft_strlen(name);
+	if (name[len - 4] != '.' || name[len - 3] != 'c' || name[len - 2] != 'u' || name[len - 1] != 'b')
+	{
+		printf("Error\nFile name is not correct. (***.cub)\n");
+		return (true);
+	}
+	return (false);
+}
+
+bool	open_check(char *file_path)
+{
+	int fd;
+
+	fd = open(file_path, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error\nCant open this file.\n");
+		return (true);
+	}
+	close(fd);
+	return (false);
+}
